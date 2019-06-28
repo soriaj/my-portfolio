b8e892e2c1f4437e9eceaa98949408e2c00b808d
Javier Soria <javier.soria@gmail.com>
2019-06-28 03:32:21 -0700

    portfolio page submission

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-function myFunction() {
-   var x = document.getElementById("menu");
-   if (x.style.display === "inline-flex") {
-     x.style.display = "none";
-   } else {
-     x.style.display = "inline-flex";
-   }
- }
+'use strict'
+
+let menu = $('.js_menu');
+let navToggle = $('.js_nav_toggle');
+
+function toggleMenu() {
+  navToggle.on('click', function(event){
+    console.log(`you clicked the right button`);
+    menu.toggleClass('active');
+  });
+}
+
+function initPage(){
+  toggleMenu();
+}
+
+$(initPage);
