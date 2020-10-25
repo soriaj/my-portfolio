ea21265ce52f767f93c15df404a0fb3e93fbcf06
Javier Soria <javier.soria@gmail.com>
2020-10-25 15:47:53 -0700

    - Add project images - Update project links - Update sidenav menu - Update javascript listeners

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 'use strict'
 
 function initPage(){
+  sideNavSmoothScroll();
+  pageNaveSmoothScroll();
   openNav();
   closeNav();
 }
 
-function openNav() {
-  document.getElementById("mySidenav").style.width = "100%";
+function sideNavSmoothScroll() {
+  $(".sidenav a").on('click', function(event) {
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+      // Animate Scroll
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800);
+    }
+    return false;
+  });
 }
 
-/* Close/hide the sidenav */
+function pageNaveSmoothScroll() {
+  $(".fullpage_nav a").on('click', function(event) {
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+      // Animate Scroll
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800);
+    }
+    return false;
+  });
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.querySelector(".main").style.marginRight = "250px";
+  document.querySelector(".main_nav_hamburger").style.display = "none"
+}
+
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  document.querySelector(".main").style.marginRight = "0";
+  document.querySelector(".main_nav_hamburger").style.display = "block"
 }
 
 $(initPage);
